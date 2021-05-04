75654231d448b79a6fe18f8d0943ba6db236927a
Izanaaagi <vlad.rotar2@gmail.com>
2021-05-05 02:00:39 +0300

    server: fix register role bug

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -59,7 +59,7 @@ class RegisterController extends Controller
             'email' => $request['email'],
             'password' => Hash::make($request['password']),
         ]);
-        $user->assignRole('User');
+        $user->assignRole('user');
         return $user;
     }
 }
